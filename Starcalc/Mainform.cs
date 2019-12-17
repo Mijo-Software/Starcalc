be6c881fa124e40f9854a4651d4321b2d07c37f6
Michael Johne <mjohne@users.noreply.github.com>
2019-12-17 21:15:55 +0100

    Add files via upload

---
+++ Starcalc/Mainform.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Starcalc
+{
+	public partial class Mainform : Form
+	{
+		public Mainform()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Starcalc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Starcalc
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+			Application.Run(mainForm: new Mainform());
+		}
+	}
+}
